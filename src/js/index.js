9cdb037472c2837a240fc98d4b73a3b47fc47d06
Caleb Anderson <buyananderson@gmail.com>
2021-08-04 19:19:46 +0200

    Extracting the fuction of sending a get request to our API

--- src/js/index.js
+++ src/js/index.js
@@ -7,12 +7,16 @@ const inputScore = document.querySelector('.inputScore');
 const refrech = document.querySelector('.refrech');
 
 refrech.addEventListener('click', async () => {
-  const result = await fetch(
-    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/zaEndvNXcETRUSYvVVNr/scores'
-  );
+  const result = await getScores();
   const { result: data } = await result.json();
   display(data);
 });
+async function getScores() {
+  const result = await fetch(
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/zaEndvNXcETRUSYvVVNr/scores'
+  );
+  return result;
+}
 
 form.addEventListener('submit', async (e) => {
   e.preventDefault();
