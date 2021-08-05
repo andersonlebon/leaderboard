9bd82580d9cdb3b192b39297e63194c5532b5b24
Caleb Anderson <buyananderson@gmail.com>
2021-08-05 11:42:29 +0200

    Style te refrech button

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,47 @@
-import '../sass/style.scss';
-import getResponse, { display, getScores } from './data';
+// import '../sass/style.scss';
+// import getResponse, { display, getScores } from './data';
 
 const form = document.querySelector('form');
 const inputName = document.querySelector('.inputName');
 const inputScore = document.querySelector('.inputScore');
 const refrech = document.querySelector('.refrech');
+
+const display = (data) => {
+  const tableBody = document.querySelector('.table-body');
+  tableBody.innerHTML = '';
+  data.forEach((player) => {
+    const tr = document.createElement('tr');
+    tr.innerHTML = `<td>${player.user} : ${player.score}</td>`;
+    tableBody.appendChild(tr);
+  });
+};
+
+// form data
+const API =
+  'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/bwkANb7WQHsP46ADhdAE/scores';
+
+const getScores = async () => {
+  const result = await fetch(API);
+  return result;
+};
+
+const sendPostRequest = async (url, newData) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newData),
+  });
+  return response.json();
+};
+
+const getResponse = async (newUser) => {
+  const data = await sendPostRequest(API, newUser);
+  const newData = await data;
+  return newData;
+};
+// form data
 
 refrech.addEventListener('click', async () => {
   const result = await getScores();
